b1127714dae0e035215d75f899e886780234b59f
github8819 <140045439+github8819@users.noreply.github.com>
2023-07-26 09:33:23 +0530

    Update streamlit_app.py
    own fruit details

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,4 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.text('I love Sunita')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
