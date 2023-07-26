0ffc51c373db2f1e82a40a30c691889982393a64
github8819 <140045439+github8819@users.noreply.github.com>
2023-07-26 09:16:20 +0530

    Update streamlit_app.py
    streamlit reflect changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ streamlit.title('My parents having healthy dinner')
 streamlit.header('Breakfast menu')
 streamlit.text('Bara, idli, Samosa')
 streamlit.text('Rice,Dal,Santula')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
