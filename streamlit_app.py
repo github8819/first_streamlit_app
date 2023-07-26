cf84ff66ba9744c61376b010eb7cebaa06c2a8d7
github8819 <140045439+github8819@users.noreply.github.com>
2023-07-26 09:40:10 +0530

    Update streamlit_app.py
    Pandas used

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,5 @@
 import streamlit
 streamlit.title('My parents having healthy dinner')
-streamlit.header('Breakfast menu')
-streamlit.text('Bara, idli, Samosa')
-streamlit.text('Rice,Dal,Santula')
-
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
@@ -11,3 +7,7 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.text('I love Sunita')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
