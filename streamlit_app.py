d68b3aa2e4e00daf3dce31ec7d0487ffcb14df96
github8819 <140045439+github8819@users.noreply.github.com>
2023-07-31 10:19:34 +0530

    Update streamlit_app.py
    Fruit choice to search

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,10 +24,12 @@ streamlit.dataframe(fruit_to_show)
 #New section to dispaly Fruitcive API response
 
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"fruit_choice")
 #streamlit.text(fruityvice_response)
 #streamlit.text(fruityvice_response.json()) #Just write the data in screen
 
