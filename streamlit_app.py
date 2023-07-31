576931f59c4f0a457df50e39b114c7c81a34fab7
github8819 <140045439+github8819@users.noreply.github.com>
2023-07-31 10:14:39 +0530

    Update streamlit_app.py
    Change the data retrieve command

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,8 @@ streamlit.dataframe(fruit_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 #streamlit.text(fruityvice_response)
 streamlit.text(fruityvice_response.json()) #Just write the data in screen
 
