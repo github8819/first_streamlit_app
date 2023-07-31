6680309fe5fa6d094b0def08a783128967441e34
github8819 <140045439+github8819@users.noreply.github.com>
2023-07-31 10:21:04 +0530

    Update streamlit_app.py
    Remove double quote

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"fruit_choice")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response)
 #streamlit.text(fruityvice_response.json()) #Just write the data in screen
 
