0af85d6555162648dd8ebdbdcf632e58618e979a
github8819 <140045439+github8819@users.noreply.github.com>
2023-08-02 10:28:45 +0530

    Update streamlit_app.py
    comment of streamlit stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 #Do not run anything past here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -57,4 +57,4 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit= streamlit.text_input('What fruit would you like to add it ?')
 streamlit.write('Thanks for adding ', add_my_fruit)
 #my_cur.execute("insert into fruit_load_list values ('" + add_my_fruit + "')")
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
